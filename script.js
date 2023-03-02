4ee85d371aa3162285c6a116ad0f284fbc0756d7
Lasha.KH <60621148+laash@users.noreply.github.com>
2023-03-02 20:15:07 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function addCard() {
-     // Get user inputs
      let fullName = document.getElementById("fullName").value;
      let age = document.getElementById("age").value;
      let address = document.getElementById("address").value;
      let gender = document.getElementById("gender").value;
 
-     // Create card element
      let card = document.createElement("div");
      card.className = "card";
 
-     // Create card content
      let content = document.createElement("p");
      content.innerHTML =
           "<strong>Name:</strong> " +
@@ -21,14 +18,11 @@ function addCard() {
           "<br><strong>Gender:</strong> " +
           gender;
 
-     // Add content to card
      card.appendChild(content);
 
-     // Add card to card container
      let cardContainer = document.getElementById("cardContainer");
      cardContainer.appendChild(card);
 
-     // Clear input fields
      document.getElementById("fullName").value = "";
      document.getElementById("age").value = "";
      document.getElementById("address").value = "";
